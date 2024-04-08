ceabb311761a53a11cbd681d26fa1a07b66925e2
Babak Farkhoopak <44144724+babakfp@users.noreply.github.com>
2024-04-08 18:07:51 +0330

    Replaced Custom CSS field textarea with file input

--- src/utilities/FormSchema.ts
+++ src/utilities/FormSchema.ts
@@ -1,11 +1,24 @@
 import * as v from "@valibot/valibot"
+import { FormFile, isFormFile } from "multipart"
 
 export const validateForm = (value: unknown) => v.safeParse(FormSchema, value)
 
 const FormSchema = v.optional(
     v.object({
         font: v.optional(v.string()),
-        css: v.optional(v.string()),
+        css: v.optional(
+            v.transform(
+                v.special<FormFile>((input) => {
+                    if (input === "") return true
+                    if (!isFormFile(input)) return false
+                    return input.type === "text/css"
+                }),
+                (input) => {
+                    const DECODER = new TextDecoder()
+                    return DECODER.decode(input.content)
+                }
+            )
+        ),
         backup: v.optional(
             v.special<boolean>((input) => typeof input === "string")
         ),
