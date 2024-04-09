485eaf9c58e8d210252e734187625d8f9225a6d0
Babak Farkhoopak <44144724+babakfp@users.noreply.github.com>
2024-04-09 16:31:50 +0330

    Removed bloat

--- src/lib/abcEdgeRenderer.ts
+++ src/lib/abcEdgeRenderer.ts
@@ -4,14 +4,11 @@ import { join } from "@std/path/join"
 
 const edge = new Edge()
 
-const edgeRender = async (content: string, data: Record<string, unknown>) =>
-    await edge.renderRaw(content, data)
-
 // TODO: Remove `Renderer` if the below `@ts-expect-error` gets fixed.
 export const abcEdgeRenderer: Renderer = {
     // @ts-expect-error They use old version of Deno and there is nothing that I can do!
     render: async (path, data: Record<string, unknown>) => {
         const content = Deno.readTextFileSync(join(Deno.cwd(), path))
-        return await edgeRender(content, data)
+        return await edge.renderRaw(content, data)
     },
 }
