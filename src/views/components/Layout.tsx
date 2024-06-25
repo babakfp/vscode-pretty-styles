5e298fe0b6de97aad9cc023aaaffbc07de09899b
babakfp <babak.bxf@gmail.com>
2024-06-25 22:57:08 +0330

    Fixed TSX types

--- src/views/components/Layout.tsx
+++ src/views/components/Layout.tsx
@@ -1,20 +1,20 @@
-export default (props: { title: string; children: JSX.Element }) => {
-    return (
-        <html lang="en">
-            <head>
-                <meta charset="utf-8" />
-                <meta name="viewport" content="width=device-width" />
-                <meta name="color-scheme" content="light dark" />
-                <link rel="icon" href="/favicon.svg" />
-                <link rel="stylesheet" href="/npm/@picocss/pico@2.0.6.css" />
-                <link
-                    rel="stylesheet"
-                    href="/npm/@picocss/pico@2.0.6/css/pico.colors.min.css"
-                />
-                <link rel="stylesheet" href="/index.css" />
-                <title>{props.title}</title>
-            </head>
-            <body>{props.children}</body>
-        </html>
-    )
-}
+import type { PropsWithChildren } from "@hono/hono/jsx"
+
+export const Layout = (props: PropsWithChildren<{ title: string }>) => (
+    <html lang="en">
+        <head>
+            <meta charset="utf-8" />
+            <meta name="viewport" content="width=device-width" />
+            <meta name="color-scheme" content="light dark" />
+            <link rel="icon" href="/favicon.svg" />
+            <link rel="stylesheet" href="/npm/@picocss/pico@2.0.6.css" />
+            <link
+                rel="stylesheet"
+                href="/npm/@picocss/pico@2.0.6/css/pico.colors.min.css"
+            />
+            <link rel="stylesheet" href="/index.css" />
+            <title>{props.title}</title>
+        </head>
+        <body>{props.children}</body>
+    </html>
+)
