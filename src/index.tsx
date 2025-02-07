81afea28e9790f93c25f5cccfd80dafb96b31972
babakfp <babak.bxf@gmail.com>
2025-02-08 01:12:19 +0330

    remove hono

--- src/index.tsx
+++ src/index.tsx
@@ -1,113 +1,175 @@
 import { getAvailablePort } from "@std/net"
-import { STATUS_CODE } from "@std/http/status"
-import { Hono } from "@hono/hono"
-import { getCookie, setCookie } from "@hono/hono/cookie"
-import { validator } from "@hono/hono/validator"
-import { serveStatic } from "@hono/hono/deno"
+import { STATUS_CODE, STATUS_TEXT } from "@std/http/status"
+import { getCookies, setCookie } from "@std/http/cookie"
 import * as v from "@valibot/valibot"
 import { FormSchema } from "./utils/FormSchema.ts"
 import { updateVsCodeStyles } from "./utils/updateVsCodeStyles.ts"
 import Index from "./views/pages/Index.tsx"
 import embed from "../embed/public/dir.ts"
+import { type Route, route } from "@std/http/unstable-route"
+import { serveDir } from "@std/http/file-server"
+import { render } from "preact-render-to-string"
+import { contentType } from "@std/media-types"
 
-const app = new Hono()
+const getCookie = (
+    headers: Headers,
+    cookieName: string,
+) => {
+    return getCookies(headers)[cookieName] ?? ""
+}
 
-app.get("/", (c) => {
-    const font = decodeURIComponent(
-        getCookie(c, "vscode-custom-styles-font") ?? "",
+const createHTMLResponse = (body?: BodyInit | null, init?: ResponseInit) => {
+    return new Response(
+        body,
+        {
+            ...init,
+            headers: {
+                ...init?.headers,
+                "Content-Type": contentType("text/html"),
+            },
+        },
     )
+}
 
-    return c.html("<!DOCTYPE html>" + <Index font={font} />)
-})
+const routes: Route[] = [
+    {
+        pattern: new URLPattern({ pathname: "/" }),
+        handler: (request) => {
+            const font = decodeURIComponent(getCookie(
+                request.headers,
+                "vscode-custom-styles-font",
+            ))
 
-app.post(
-    "/",
-    validator("form", async (value, c) => {
-        const isFormValid = v.safeParse(FormSchema, value)
-
-        if (!isFormValid.success) {
-            return c.html(
-                "<!DOCTYPE html>" +
-                    <Index statusText="Invalid data submitted!" />,
-                STATUS_CODE.BadRequest,
+            return createHTMLResponse(
+                render(<Index font={font} />),
             )
-        }
+        },
+    },
+    {
+        method: ["POST"],
+        pattern: new URLPattern({ pathname: "/" }),
+        handler: async (request) => {
+            const formData = await request.formData()
 
-        const formData = isFormValid.output
+            const isFormValid = v.safeParse(
+                FormSchema,
+                Object.fromEntries(formData),
+            )
 
-        if (!formData?.backup && !formData?.font && !formData?.css) {
-            return c.html(
-                "<!DOCTYPE html>" +
-                    (
+            if (!isFormValid.success) {
+                const font = decodeURIComponent(getCookie(
+                    request.headers,
+                    "vscode-custom-styles-font",
+                ))
+
+                return createHTMLResponse(
+                    render(
+                        <Index
+                            statusText="Invalid data submitted!"
+                            font={font}
+                        />,
+                        { status: STATUS_CODE.BadRequest },
+                    ),
+                )
+            }
+
+            const validatedFormData = isFormValid.output
+
+            const headers = new Headers()
+            if (validatedFormData?.font) {
+                setCookie(headers, {
+                    name: "vscode-custom-styles-font",
+                    value: encodeURIComponent(validatedFormData.font),
+                })
+            }
+
+            if (
+                !validatedFormData?.backup && !validatedFormData?.font &&
+                !validatedFormData?.css
+            ) {
+                return createHTMLResponse(
+                    render(
                         <Index
                             statusCode={STATUS_CODE.BadRequest}
                             statusText='"Editor UI Font-Family" or "Custom CSS" cannot be empty!'
-                            font={formData?.font}
-                        />
+                            font={validatedFormData?.font}
+                        />,
+                        { status: STATUS_CODE.BadRequest, headers },
                     ),
-            )
-        }
+                )
+            }
 
-        if (formData?.font) {
-            setCookie(
-                c,
-                "vscode-custom-styles-font",
-                encodeURIComponent(formData.font),
-            )
-        }
+            const result = await updateVsCodeStyles({
+                ...validatedFormData,
+                css: validatedFormData.css instanceof File
+                    ? await validatedFormData.css.text()
+                    : validatedFormData.css,
+            })
 
-        const result = await updateVsCodeStyles({
-            ...formData,
-            css: formData.css instanceof File
-                ? await formData.css.text()
-                : formData.css,
-        })
+            let statusText: string
 
-        let statusText: string
+            if (result.type === "ERROR") {
+                statusText = result.message
+            } else if (validatedFormData?.backup) {
+                statusText = "Original styles restored successfully!"
+            } else {
+                statusText = "Custom styles were added successfully!"
+            }
 
-        if (result.type === "ERROR") {
-            statusText = result.message
-        } else if (formData?.backup) {
-            statusText = "Original styles restored successfully!"
-        } else {
-            statusText = "Custom styles were added successfully!"
-        }
+            const status = result.type === "ERROR"
+                ? STATUS_CODE.BadRequest
+                : STATUS_CODE.OK
 
-        const status = result.type === "ERROR"
-            ? STATUS_CODE.BadRequest
-            : STATUS_CODE.OK
-
-        return c.html(
-            "<!DOCTYPE html>" +
-                (
+            return createHTMLResponse(
+                render(
                     <Index
                         statusCode={status}
                         statusText={statusText}
-                        font={formData?.font}
-                    />
+                        font={validatedFormData?.font}
+                    />,
+                    { status, headers },
                 ),
-            status,
-        )
-    }),
-)
+            )
+        },
+    },
+]
 
 if (Deno.args.includes("--compile")) {
-    app.get("**", async (c) => {
-        const url = new URL(c.req.url)
-        for (const path of embed.list()) {
-            if (url.pathname === `/${path}`) {
-                const file = await embed.get(path)
-                const bytes = await file!.bytes()
-                return c.newResponse(bytes)
+    routes.push({
+        pattern: new URLPattern({ pathname: "*" }),
+        handler: async (request) => {
+            const url = new URL(request.url)
+            for (const path of embed.list()) {
+                if (url.pathname === `/${path}`) {
+                    const file = await embed.get(path)
+                    const bytes = await file!.bytes()
+                    return new Response(bytes)
+                }
             }
-        }
-        return c.notFound()
+
+            return new Response(undefined, {
+                status: STATUS_CODE.NotFound,
+            })
+        },
     })
 } else {
-    app.use("*", serveStatic({ root: "public" }))
+    routes.push({
+        // Prefix it with puglic
+        pattern: new URLPattern({ pathname: "*" }),
+        handler: (req: Request) =>
+            serveDir(req, {
+                fsRoot: "/public",
+            }),
+    })
 }
 
 const port = Deno.args.includes("--compile") ? getAvailablePort() : 3000
+
+const defaultHandler = (_req: Request) => {
+    return new Response(STATUS_TEXT[STATUS_CODE.NotFound], {
+        status: STATUS_CODE.NotFound,
+    })
+}
 
 Deno.serve(
     {
@@ -125,5 +187,5 @@ Deno.serve(
             }
         },
     },
-    app.fetch,
+    route(routes, defaultHandler),
 )
--- src/views/components/Layout.tsx
+++ src/views/components/Layout.tsx
@@ -1,27 +1,30 @@
-import type { PropsWithChildren } from "@hono/hono/jsx"
+export const Layout = (
+    props: { title: string; children: preact.VNode },
+) => (
+    <>
+        {"<!DOCTYPE html>"}
+        <html lang="en">
+            <head>
+                <meta charset="utf-8" />
+                <meta name="viewport" content="width=device-width" />
+                <meta name="color-scheme" content="light dark" />
 
-export const Layout = (props: PropsWithChildren<{ title: string }>) => (
-    <html lang="en">
-        <head>
-            <meta charset="utf-8" />
-            <meta name="viewport" content="width=device-width" />
-            <meta name="color-scheme" content="light dark" />
+                <title>{props.title}</title>
+                <link rel="icon" href="/icon.ico" />
 
-            <title>{props.title}</title>
-            <link rel="icon" href="/icon.ico" />
+                <link
+                    rel="preload"
+                    href="/fonts/GeistMonoVF.woff2"
+                    as="font"
+                    type="font/woff2"
+                    crossorigin="anonymous"
+                />
 
-            <link
-                rel="preload"
-                href="/fonts/GeistMonoVF.woff2"
-                as="font"
-                type="font/woff2"
-                crossorigin="anonymous"
-            />
-
-            <link rel="stylesheet" href="/css/pico@2.0.6.css" />
-            <link rel="stylesheet" href="/css/pico@2.0.6.colors.min.css" />
-            <link rel="stylesheet" href="/css/index.css" />
-        </head>
-        <body>{props.children}</body>
-    </html>
+                <link rel="stylesheet" href="/css/pico@2.0.6.css" />
+                <link rel="stylesheet" href="/css/pico@2.0.6.colors.min.css" />
+                <link rel="stylesheet" href="/css/index.css" />
+            </head>
+            <body>{props.children}</body>
+        </html>
+    </>
 )
