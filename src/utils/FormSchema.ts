14490983dd9092f29141662e195582c457ff672d
babakfp <babak.bxf@gmail.com>
2025-02-08 06:40:35 +0330

    stuff

--- src/utils/FormSchema.ts
+++ src/utils/FormSchema.ts
@@ -1,28 +1,26 @@
 import * as v from "@valibot/valibot"
 
-export const FormSchema = v.optional(
-    v.object({
-        font: v.optional(v.string()),
-        css: v.optional(
-            v.union([
-                v.string(),
-                v.pipe(
-                    v.custom<File>(
-                        (input) => input instanceof File,
-                        "Expected a file!",
-                    ),
-                    v.check(
-                        (input) => input.type === "text/css",
-                        "Expected a CSS file!",
-                    ),
-                ),
-            ]),
-        ),
-        backup: v.optional(
+export const FormSchema = v.object({
+    font: v.optional(v.string()),
+    css: v.optional(
+        v.union([
+            v.string(),
             v.pipe(
-                v.literal("true"),
-                v.transform(() => true),
+                v.custom<File>(
+                    (input) => input instanceof File,
+                    "Expected a file!",
+                ),
+                v.check(
+                    (input) => input.type === "text/css",
+                    "Expected a CSS file!",
+                ),
             ),
+        ]),
+    ),
+    backup: v.optional(
+        v.pipe(
+            v.literal("true"),
+            v.transform(() => true),
         ),
-    }),
-)
+    ),
+})
