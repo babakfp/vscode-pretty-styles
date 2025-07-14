e58b40b19b3e481a86f39ef0d2c5dcb66859a90f
Babak Farkhoopak <44144724+babakfp@users.noreply.github.com>
2025-07-14 05:43:45 +0330

    chore: log exit message before exit

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { serve } from "./utilities/serve.tsx"
 
 if (Deno.build.os !== "windows") {
+    console.log("This script only works on Windows!")
     Deno.exit()
 }
 
 const homeDir = Deno.env.get("USERPROFILE")
 
 if (!homeDir) {
+    console.log("Could not find home dir!")
     Deno.exit()
 }
 
